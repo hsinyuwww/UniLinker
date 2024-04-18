2d59e1872c4cc8dc05ce07d0c8a74647098cd469
Bini-C <simple.bini@gmail.com>
2024-04-18 13:20:26 -0400

    Added upload profile picture functionality -camera, gallery, remove picture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.firebase:firebase-storage")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     // Import the BoM for the Firebase platform
