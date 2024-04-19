e1aee7472fa9b99e408d74214b81b0d64361d608
Bini-C <simple.bini@gmail.com>
2024-04-19 01:36:10 -0400

    added user profiles viewing functionality

--- app/src/main/java/edu/northeastern/numad24sp_group4unilink/Attendees/AttendeesAdapter.java
+++ app/src/main/java/edu/northeastern/numad24sp_group4unilink/Attendees/AttendeesAdapter.java
@@ -1,5 +1,6 @@
 package edu.northeastern.numad24sp_group4unilink.Attendees;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 import edu.northeastern.numad24sp_group4unilink.R;
-
+import edu.northeastern.numad24sp_group4unilink.events.ViewEventActivity;
+import edu.northeastern.numad24sp_group4unilink.profile.ProfileActivity;
 
 
 public class AttendeesAdapter extends RecyclerView.Adapter<AttendeesHolder>{
@@ -40,6 +42,14 @@ public class AttendeesAdapter extends RecyclerView.Adapter<AttendeesHolder>{
 
 
         holder.userEmail.setText(currentItem.getUserEmail());
+        holder.userEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ProfileActivity.class);
+                intent.putExtra("email", holder.userEmail.getText().toString());
+                v.getContext().startActivity(intent);
+            }
+        });
 
     }
 
